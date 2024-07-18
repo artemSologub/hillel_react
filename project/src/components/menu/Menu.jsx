db9ce18c4177df425fef62815bf34326152ffd51
artemSologub <artem.sologub92@gmail.com>
2024-07-18 23:47:55 +0300

    Data From API.

--- project/src/components/menu/Menu.jsx
+++ project/src/components/menu/Menu.jsx
@@ -1,21 +1,22 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
-import { pizzas } from '../../mockData/data';
+import useFetch from '../../hooks/use-fetch';
 import MenuList from '../menuList/MenuList';
 
 const Menu = () => {
   const [items, setItems] = useState([]);
-
-  const getData = useCallback(() => {
-    setItems(pizzas);
-  }, []);
+  const { data, error, isLoading } = useFetch('https://react-fast-pizza-api.onrender.com/api/menu');
 
   useEffect(() => {
-    getData();
-  }, [getData]);
+    if (data) {
+      setItems(data.data);
+    }
+  }, [data]);
 
   return (
     <>
+      {isLoading && <p>Loading...</p>}
+      {error && <p>Error: {error.message}</p>}
       <MenuList items={items} />
     </>
   );
